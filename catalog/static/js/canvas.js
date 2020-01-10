6dc50bc99d38fbec24dd0c1288de90fbcffb3dbc
Motokulman <2594501@mail.ru>
2020-01-10 19:23:52 +0300

    масштабирование с глюком

--- catalog/static/js/canvas.js
+++ catalog/static/js/canvas.js
@@ -9,7 +9,7 @@ var mousePos; // Позиции мыши по х и у, с учетом поло
 var points = []; // Массив точек в миллиметрах. Первая точка - точка отсчета, начало коопдинат
 var zeroPointPadding = []; // Смещение начала координат схемы относительно начала координат канвы. Попробуем в мм.
 var walls = []; // Массив стен
-var scaling = 25; // Сделать получение из настроек и сохранение в них
+var scale = 25; // Сделать получение из настроек и сохранение в них
 var empty_scheme = true;// Правда, если еще нет ни одного элемента в схеме
 
 
@@ -36,7 +36,6 @@ function drawHVLine(type) {
     ctx_1.stroke();
 }
 
-
 // рисуем точку
 function drawPoint(p) {
     ctx_0.beginPath();
@@ -52,12 +51,12 @@ canvas_0.addEventListener('click', function (e) {
     if (selectedTool != 'none') { // если хоть что то выбрано
         if (empty_scheme) { // если это первая точка в схеме, то она становится центром координат
             points.push([0, 0, 0]);
-            zeroPointPadding.x = mousePos.x;
-            zeroPointPadding.y = mousePos.y;
+            zeroPointPadding.x = mousePos.x * scale;
+            zeroPointPadding.y = mousePos.y * scale;
             empty_scheme = false;
             //console.log("zeroPointPadding = ", zeroPointPadding);
         } else {
-            points.push([points[points.length - 1][0] + 1, (mousePos.x - zeroPointPadding.x) * scaling, (mousePos.y - zeroPointPadding.y) * scaling]); // переводим в мм и вносим в массив
+            points.push([points[points.length - 1][0] + 1, (mousePos.x * scale - zeroPointPadding.x), (mousePos.y * scale - zeroPointPadding.y)]); // переводим в мм и вносим в массив
             //  console.log("points = ", points);
         }
         stick();
@@ -83,7 +82,7 @@ canvas_0.addEventListener('click', function (e) {
 
             }
             //console.log("Выбрана стена");
-            break;  
+            break;
         case 'none':
             //console.log("Ничего не выбрано");
             break;
@@ -103,8 +102,8 @@ function stick() {
     // Перебор всех точек
     for (item of points.values()) {
         var a = [];
-        a.x = item[1] / scaling + zeroPointPadding.x;
-        a.y = item[2] / scaling + zeroPointPadding.y;
+        a.x = mmToPix(item[1], item[2]).x;
+        a.y = mmToPix(item[1], item[2]).y;
         if (Math.abs(mousePos.x - a.x) <= stick_pix) {
             mousePos.x = a.x;
             drawHVLine("v");
@@ -116,21 +115,18 @@ function stick() {
     }
 }
 
+// Перевод миллиметров в пиксели
+function mmToPix(x, y) {
+    return {
+        x: (x + zeroPointPadding.x) / scale,
+        y: (y + zeroPointPadding.y) / scale
+    };
+}
+
 
 // Проверочные действия
 $('#test_buttons button').click(function () {
-    clear(ctx_0, canvas_0);
-    for (item of walls.values()) {
-        var a = [];
-        var b = [];
-        a.x = item[0][0] / scaling + zeroPointPadding.x;
-        a.y = item[0][1] / scaling + zeroPointPadding.y;
-        b.x = item[1][0] / scaling + zeroPointPadding.x;
-        b.y = item[1][1] / scaling + zeroPointPadding.y;
-        console.log("a = ", a);
-        console.log("b = ", b);
-        drawLine(a, b, ctx_0);
-    }
+    drawWalls();
 });
 
 
@@ -164,4 +160,97 @@ canvas_0.addEventListener('mousemove', function (e) {
     //console.log("mousePos 0= ", mousePos);
 });
 
+// Воспроизведение из массива стен
+function drawWalls() {
+    clear(ctx_0, canvas_0);
+    for (wall of walls.values()) {
+        for (point of points.values()) {
+            if (wall[0] == point[0]) {
+                drawPoint(mmToPix(point[1], point[2]));
+            }
+            if (wall[1] == point[0]) {
+                drawPoint(mmToPix(point[1], point[2]));
+            }
+        }
+    }
+}
 
+
+
+// Масштабирование колесиком мыши
+var elem = document.getElementById('stage');
+
+if (elem.addEventListener) {
+    if ('onwheel' in document) {
+        // IE9+, FF17+
+        elem.addEventListener("wheel", onWheel);
+    } else if ('onmousewheel' in document) {
+        // устаревший вариант события
+        elem.addEventListener("mousewheel", onWheel);
+    } else {
+        // Firefox < 17
+        elem.addEventListener("MozMousePixelScroll", onWheel);
+    }
+} else { // IE8-
+    elem.attachEvent("onmousewheel", onWheel);
+}
+
+function onWheel(e) {
+    e = e || window.event;
+    var delta = e.deltaY || e.detail || e.wheelDelta;
+    e.preventDefault ? e.preventDefault() : (e.returnValue = false);
+    var scale_old = scale;
+    scale = (parseFloat(scale) + parseFloat(delta / 100)).toFixed(1);
+    if (scale == 0) scale = 0.001; 
+    
+    if (scale_old > scale) {
+        //console.log("================");
+        //console.log("zeroPointPadding.x = ", zeroPointPadding.x);
+        //console.log("scale_old = ", scale_old);
+        //console.log("(mousePos.x= ",  mousePos.x);
+        console.log("scale = ", scale);
+    
+        zeroPointPadding.x = Math.round((zeroPointPadding.x/scale_old + (zeroPointPadding.x/scale_old - mousePos.x)/scale)* scale);
+        console.log("zeroPointPadding.x = ", zeroPointPadding.x);
+        zeroPointPadding.y = Math.round((zeroPointPadding.y/scale_old + (zeroPointPadding.y/scale_old - mousePos.y)/scale) * scale);
+    } else {
+        zeroPointPadding.x = Math.round((zeroPointPadding.x/scale_old - (zeroPointPadding.x/scale_old - mousePos.x)/scale)* scale);
+        zeroPointPadding.y = Math.round((zeroPointPadding.y/scale_old - (zeroPointPadding.y/scale_old - mousePos.y)/scale) * scale);
+    }
+
+
+        // zeroPointPadding.x = zeroPointPadding.x - (zeroPointPadding.x/scale_old - mousePos.x) * scale;
+    // zeroPointPadding.y = zeroPointPadding.y - (zeroPointPadding.y/scale_old - mousePos.y) * scale;
+    drawWalls();
+}
+
+
+// canvas_0.addEventListener("wheel", function (e) {
+//     e = e || window.event;
+//       scale = (parseFloat(scale) + parseFloat(e.deltaY / 100)).toFixed(1);
+//       console.log("scale = ", scale);
+//   });
+
+// Добавления полей редактирования 
+// function addSizeInput(top, left, inputField, id, size) {
+//     var elem = document.createElement("input");
+//     var topStr = String(top) + "px";
+//     var leftStr = String(left) + "px";
+//     elem.id = id;
+//     elem.className = "SizeInput";
+//     elem.type = "number";
+//     elem.min = 0;
+//     elem.max = 90000;
+//     elem.pattern = "[0-9]{0,5}";
+//     elem.inputmode = "numeric";
+//     elem.maxLength = 5;
+//     elem.value = size;
+//     //console.log("id = ", id);
+//     elem.style.top = topStr;
+//     elem.style.left = leftStr;
+//     elem.style.position = "relative";
+//     elem.style.width = String(sizeInputWidth) + "px";
+//     elem.style.height = String(sizeInputHeight) + "px";
+//     elem.style.webkitAppearance = none;
+//     inputField.appendChild(elem);
+//   }
