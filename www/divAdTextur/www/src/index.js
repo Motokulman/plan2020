c91175a8e55f4cfe181d96260aab24a5176f9673
vorodis2 <vorodis2@gmail.com>
2020-12-02 12:21:05 +0200

    1

--- www/divAdTextur/www/src/index.js
+++
@@ -1,3 +0,0 @@
-import { Main} from './bilding/Main.js';
-global.Main = Main;
-export { Main };
--- www/divAdTextur/www/src/libMy/visi3D_1/index.js
+++
@@ -1,8 +0,0 @@
-
-import { MVisi3D } from './MVisi3D.js';
-
-
-global.MVisi3D = MVisi3D;
-
-
-export {MVisi3D};
